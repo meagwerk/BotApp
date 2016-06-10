130a21fda25a8b542250e7e6fd920300fc8dcfd4
Dmitry Kochikiyan <dmitry.kochikiyan@tum.de>
2016-06-10 13:27:49 +0200

    Conversations Model Commit

---
+++ MyProjekt/Models/Conversations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProjekt.Models
+{
+    public class Conversations
+    {
+        public int ID { get; set; }
+        public int Status { get; set; } //0 - ongoing, 1 - finished, 2 - continuation pending
+        public int Score { get; set; } //bot evaluation score
+        public DateTime MessageDate { get; set; }
+        public String Messages { get; set; }
+
+        /*  public Conversations(int id, int status, int score, DateTime messageDate, string messages)
+          {
+              this.ID = id;
+              this.Status = status;
+              this.MessageDate = messageDate;
+              this.Messages = messages;
+          }*/
+    }
+}
