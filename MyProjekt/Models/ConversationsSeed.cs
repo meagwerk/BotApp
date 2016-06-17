2ed6a9243b5eb0f0d02483c89603a7a8a9c872f1
Dmitry Kochikiyan <dmitry.kochikiyan@tum.de>
2016-06-17 14:34:15 +0200

    Added support for DB - still needs work

---
+++ MyProjekt/Models/ConversationsSeed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyProjekt.Models
+{
+    public class ConversationsSeed : System.Data.Entity.DropCreateDatabaseIfModelChanges<ConversationsContext>
+    {
+        protected override void Seed(ConversationsContext context)
+        {
+            Conversations testConvers = new Conversations { ID = 0, Status = 0, Score = 0, MessageDate = DateTime.Now, Messages = "Test" };
+
+            context.Conversations.Add(testConvers);
+            context.SaveChangesAsync();
+            base.Seed(context);
+        }
+    }
+}
